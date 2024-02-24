fbe0afbfa206478d6429f34ada30ea11e7a0a742
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-24 14:14:36 +0000

    AndroidProducts: adapt to rising

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond1lte.mk
+    $(LOCAL_DIR)/rising_beyond1lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond1lte-eng \
-    lineage_beyond1lte-user \
-    lineage_beyond1lte-userdebug
+    rising_beyond1lte-eng \
+    rising_beyond1lte-user \
+    rising_beyond1lte-userdebug
